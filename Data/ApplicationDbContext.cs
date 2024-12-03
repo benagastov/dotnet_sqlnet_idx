b13fcc6fa53d94ab687aaff53e9944fa9e3f7ee3
QOQORO-IT <fanidownload@gmail.com>
2024-12-03 16:02:14 +0700

    Initial commit

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,21 +9,7 @@ public class ApplicationDbContext : DbContext
         : base(options)
     {
         Database.EnsureCreated();
-        
-        // Add this line to print the database path
-        Console.WriteLine($"Database path: {Database.GetConnectionString()}");
-        
-        // Add seed data if the database is empty
-        if (!Todos.Any())
-        {
-            Todos.AddRange(
-                new Todo { Title = "Learn C#", IsComplete = false },
-                new Todo { Title = "Build a Todo App", IsComplete = false },
-                new Todo { Title = "Master Entity Framework", IsComplete = false }
-            );
-            SaveChanges();
-        }
     }
 
-    public DbSet<Todo> Todos { get; set; } = null!;
+    public DbSet<Query> Queries { get; set; } = null!;
 } 
